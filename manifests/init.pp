eaf5e3d931bfa423250a11e72873392ea9e162e3
HawtDogFlvrWtr <hawtdogflvrwtr@gmail.com>
2015-05-11 21:20:18 -0400

    cleaned up the init file with puppet-lints help

--- manifests/init.pp
+++ manifests/init.pp
@@ -48,73 +48,49 @@
 #
 
 class skyline (
-  $username           = 'skyline',
-  $group              = 'skyline',
-  $install_dir        = '/opt',
-  $download_url       = 'https://github.com/etsy/skyline/archive/master.zip',
-  $redis_socket_path  = '/tmp/redis.sock',
-  $log_path           = '/var/log/skyline',
-  $pid_path           = '/var/run/skyline',
-  $dump_dir           = '/var/dump',
-  $full_namespace     = 'metrics.',
-  $mini_namespace     = 'mini.',
-  $full_duration      = 86400,
-  $mini_duration      = 3600,
-  $graphite_host      = 'your_graphite_host.com',
-  $graphite_url       = "http://' + GRAPHITE_HOST + '/render/?width=1400&from=-1hour&target=%s",
-  $carbon_port        = 2003,
-  $oculus_host        = 'http://you_oculus_host.com',
-  $anomaly_dump       = 'webapp/static/dump/anomalies.json',
-  $analyzer_processes = 7,
-  $stale_period       = 500,
-  $min_tolerable_length = 1,
+  $username              = 'skyline',
+  $group                 = 'skyline',
+  $install_dir           = '/opt',
+  $download_url          = 'https://github.com/etsy/skyline/archive/master.zip',
+  $redis_socket_path     = '/tmp/redis.sock',
+  $log_path              = '/var/log/skyline',
+  $pid_path              = '/var/run/skyline',
+  $dump_dir              = '/var/dump',
+  $full_namespace        = 'metrics.',
+  $mini_namespace        = 'mini.',
+  $full_duration         = 86400,
+  $mini_duration         = 3600,
+  $graphite_host         = 'your_graphite_host.com',
+  $graphite_url          = 'http://' + GRAPHITE_HOST + '/render/?width=1400&from=-1hour&target=%s',
+  $carbon_port           = 2003,
+  $oculus_host           = 'http://you_oculus_host.com',
+  $anomaly_dump          = 'webapp/static/dump/anomalies.json',
+  $analyzer_processes    = 7,
+  $stale_period          = 500,
+  $min_tolerable_length  = 1,
   $max_tolerable_boredom = 100,
-  $boredom_set_size   = 1,
-  $canary_metric      = 'statsd.numStats',
-  $consensus          = 6,
-  $enable_second_order = 'False',
-  $enable_alerts      = 'True',
-  $horizon_ip         = '0.0.0.0',
-  $worker_processes   = 2,
-  $pickle_port        = 2024,
-  $udp_port           = 2025,
-  $chunk_size         = 10,
-  $max_queue_size     = 500,
-  $roomba_processes   = 1,
-  $roomba_grace_time  = 600,
-  $max_resolution     = 1000,
-  $webapp_ip          = '0.0.0.0',
-  $webapp_port        = 1500,
-  $skip_list          = [
-                         '',
-                        ],
-  $algorithms         = [
-                         'first_hour_average',
-                         'mean_subtraction_cumulation',
-                         'stddev_from_average',
-                         'stddev_from_moving_average',
-                         'least_squares',
-                         'grubbs',
-                         'histogram_bins',
-                         'median_absolute_deviation',
-                         'ks_test',
-                        ],
-  $alerts             = [
-                         '("skyline", "smtp", 1800),'
-                        ],
-  $hipchat_opts       = [
-                         '"rooms": { "skyline": (12345,),},', 
-                         '"color": "purple",'
-                        ],
-  $smtp_opts          = [
-                         '"sender": "skyline-alerts@etsy.com",',
-                         '"recipients": {"skyline":["email@address.com", "email2@address.com"],},'
-                        ],
-  $pagerduty_opts     = [
-                         '"subdomain": "example",',
-                         '"auth_token": "key",',
-                         '"key": "key",',
-                        ],
+  $boredom_set_size      = 1,
+  $canary_metric         = 'statsd.numStats',
+  $consensus             = 6,
+  $enable_second_order   = 'False',
+  $enable_alerts         = 'True',
+  $horizon_ip            = '0.0.0.0',
+  $worker_processes      = 2,
+  $pickle_port           = 2024,
+  $udp_port              = 2025,
+  $chunk_size            = 10,
+  $max_queue_size        = 500,
+  $roomba_processes      = 1,
+  $roomba_grace_time     = 600,
+  $max_resolution        = 1000,
+  $webapp_ip             = '0.0.0.0',
+  $webapp_port           = 1500,
+  $skip_list             = ['',],
+  $algorithms         = ['first_hour_average','mean_subtraction_cumulation','stddev_from_average','stddev_from_moving_average','least_squares','grubbs','histogram_bins','median_absolute_deviation','ks_test',],
+  $alerts             = ['("skyline", "smtp", 1800),'],
+  $hipchat_opts       = ['"rooms": { "skyline": (12345,),},','"color": "purple",'],
+  $smtp_opts          = ['"sender": "skyline-alerts@etsy.com",','"recipients": {"skyline":["email@address.com", "email2@address.com"],},'],
+  $pagerduty_opts     = ['"subdomain": "example",','"auth_token": "key",','"key": "key",',],
 ) {
     include 'archive::prerequisites'
     include 'stdlib'
@@ -129,62 +105,61 @@ class skyline (
     # Important python packages that need to be installed before the other packages.
     $pipimportant = ['numpy','scipy']
     $pippackages = ['pandas','patsy','jinja2','flask','itsdangerous','werkzeug','markupsafe','pytz','python-dateutil','six','statsmodels','msgpack-python']
-    
     # Ensure all base and pip packages are installed.
     package { $basepackages:
       ensure => 'installed',
     }
     package { $pipimportant:
-      ensure => 'installed',
+      ensure   => 'installed',
       provider => 'pip',
-      before => Package[$pippackages],
+      before   => Package[$pippackages],
     }
     package { $pippackages:
+      ensure   => 'installed',
       provider => 'pip',
-      ensure => 'installed',
-      require => [Package['python-pip'], Package['gcc'], Package['gcc-c++']],
+      require  => [Package['python-pip'], Package['gcc'], Package['gcc-c++']],
     }
- 
+
     # Have to manually install redis python module with exec because of puppet bug PUP-1073
     # http://tickets.puppetlabs.com/browse/PUP-1073
-    exec { "redis-install":
-       path => "/usr/bin:/usr/sbin:/bin",
-       command => 'pip install redis',
-       require => [Package['python-pip'], Package['gcc'], Package['gcc-c++']],
-       unless => 'pip list | grep -c "redis"',
+    exec { 'redis-install':
+      path    => '/usr/bin:/usr/sbin:/bin',
+      command => 'pip install redis',
+      require => [Package['python-pip'], Package['gcc'], Package['gcc-c++']],
+      unless  => 'pip list | grep -c "redis"',
     }
 
     # Create user/group information
     user { $username:
+      ensure  => present,
       comment => 'Skyline service account',
-      groups => 'redis',
-      ensure => present,
-      before => [Service['skyline-analyzer'],Service['skyline-horizon'],Service['skyline-webapp']],
+      groups  => 'redis',
+      before  => [Service['skyline-analyzer'],Service['skyline-horizon'],Service['skyline-webapp']],
     }
 
     # Archive module to extract the skyline zip file
     archive { 'skyline':
-      ensure => present,
-      url => $download_url,
-      target => $install_dir,
-      extension => 'zip',
-      checksum => false,
+      ensure           => present,
+      url              => $download_url,
+      target           => $install_dir,
+      extension        => 'zip',
+      checksum         => false,
       strip_components => 1,
     }
 
     # Variables for install
     $require_target = Archive['skyline']
-    $config_py = "${install_dir}/skyline/skyline-master/src/settings.py"
-    $analyzer_init = "/etc/init.d/skyline-analyzer"
-    $horizon_init = "/etc/init.d/skyline-horizon"
-    $webapp_init = "/etc/init.d/skyline-webapp"
+    $config_py      = "${install_dir}/skyline/skyline-master/src/settings.py"
+    $analyzer_init  = '/etc/init.d/skyline-analyzer'
+    $horizon_init   = '/etc/init.d/skyline-horizon'
+    $webapp_init    = '/etc/init.d/skyline-webapp'
 
     # Ensure that the folders are present for skyline to run correctly
     file { [$log_path, $pid_path, $dump_dir]:
       ensure => 'directory',
-      owner => $username,
-      group => $group,
-      mode => 755,
+      owner  => $username,
+      group  => $group,
+      mode   => '0755',
     }
 
     # Copy and install the skyline configuration file.
@@ -198,48 +173,47 @@ class skyline (
     file { $analyzer_init:
         ensure  => present,
         content => template('skyline/analyzer.init.erb'),
-        owner => 'root',
-        group => 'root',
-        mode => 0755,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
         require => $require_target,
     }
     # Copy and install the skyline horizon init file.
     file { $horizon_init:
         ensure  => present,
         content => template('skyline/horizon.init.erb'),
-        owner => 'root',
-        group => 'root',
-        mode => 0755,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
         require => $require_target,
     }
     # Copy and install the skyline webapp init file.
     file { $webapp_init:
         ensure  => present,
         content => template('skyline/webapp.init.erb'),
-        owner => 'root',
-        group => 'root',
-        mode => 0755,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
         require => $require_target,
     }
     # Change redis sock permissions if they are wrong.
     file { $redis_socket_path:
-      mode => 775,
+      mode => '0775',
     }
-    
     # Ensure services are running
     service { 'skyline-analyzer':
-      ensure => 'running',
-      enable => 'true',
+      ensure  => 'running',
+      enable  => true,
       require => [File[$analyzer_init], Package[$pippackages], Package[$pipimportant]],
     }
     service { 'skyline-horizon':
-      ensure => 'running',
-      enable => 'true',
+      ensure  => 'running',
+      enable  => true,
       require => [File[$horizon_init], Package[$pippackages], Package[$pipimportant]],
     }
     service { 'skyline-webapp':
-      ensure => 'running',
-      enable => 'true',
+      ensure  => 'running',
+      enable  => true,
       require => [File[$webapp_init], Package[$pippackages], Package[$pipimportant]],
     }
 
